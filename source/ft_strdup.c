8d3c76f465f8b2c3684dac2a3d917c1d4b613738
cnatanae <cnatanae@student.42sp.org.br>
2023-10-26 06:04:06 -0300

    Updated Strdup for better Work - OK

--- source/ft_strdup.c
+++ source/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: cnatanae <cnatanae@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/13 16:06:27 by cnatanae          #+#    #+#             */
-/*   Updated: 2023/10/19 07:59:30 by cnatanae         ###   ########.fr       */
+/*   Updated: 2023/10/23 18:05:29 by cnatanae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,11 @@ char	*ft_strdup(const char *s)
 	char	*dup;
 	int		idx;
 
-	dup = malloc (sizeof(char) * ft_strlen(s) + 1);
+	dup = ft_calloc (1, sizeof(char) * ft_strlen(s) + 1);
 	if (!dup)
 		return (NULL);
 	idx = 0;
 	while (*s)
-	{
 		dup[idx++] = *(s++);
-	}
-	dup[idx] = '\0';
 	return (dup);
 }
