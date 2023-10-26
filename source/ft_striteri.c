9547e4836b61a4b0e642cfadc840b77ccccb708c
cnatanae <cnatanae@student.42sp.org.br>
2023-10-26 06:04:42 -0300

    Updated Striteri for better Work - OK

--- source/ft_striteri.c
+++ source/ft_striteri.c
@@ -16,10 +16,7 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
 	unsigned int	idx;
 
-	idx = 0;
-	while (s[idx])
-	{
+	idx = -1;
+	while (s[++idx])
 		f(idx, &s[idx]);
-		idx++;
-	}
 }
