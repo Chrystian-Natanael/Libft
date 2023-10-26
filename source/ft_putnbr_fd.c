8964df1b343079355f393e8377500f068ae4387d
cnatanae <cnatanae@student.42sp.org.br>
2023-10-26 06:03:15 -0300

    Updated Putnbr_FD for better Work - OK

--- source/ft_putnbr_fd.c
+++ source/ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: cnatanae <cnatanae@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/20 11:52:43 by cnatanae          #+#    #+#             */
-/*   Updated: 2023/10/20 14:33:43 by cnatanae         ###   ########.fr       */
+/*   Updated: 2023/10/23 19:04:24 by cnatanae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,20 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	if (n == -2147483648)
+	long int	nb;
+
+	nb = n;
+	if (nb < 0)
 	{
 		ft_putchar_fd('-', fd);
-		ft_putchar_fd('2', fd);
-		n = 147483648;
+		nb = -nb;
 	}
-	if (n < 0)
+	if (nb < 10)
 	{
-		ft_putchar_fd('-', fd);
-		n = -n;
-	}
-	if (n < 10)
-	{
-		ft_putchar_fd (n + '0', fd);
+		ft_putchar_fd (nb + '0', fd);
 		return ;
 	}
 	else
-		ft_putnbr_fd(n / 10, fd);
-	ft_putnbr_fd (n % 10, fd);
+		ft_putnbr_fd(nb / 10, fd);
+	ft_putnbr_fd (nb % 10, fd);
 }
