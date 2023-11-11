4b0692fb7781caeb0af4c4626398f200a883785c
cnatanae <cnatanae@student.42sp.org.br>
2023-11-11 12:57:16 -0300

    lstmap - OK

--- source/ft_lstmap.c
+++ source/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: cnatanae <cnatanae@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 13:00:33 by cnatanae          #+#    #+#             */
-/*   Updated: 2023/10/26 10:27:35 by cnatanae         ###   ########.fr       */
+/*   Updated: 2023/11/11 12:18:32 by cnatanae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,25 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	*new_node;
-	t_list	*head;
+	t_list	*new_list;
+	t_list	*new;
+	t_list	*tmp;
 
-	if (!f || !del)
-		return (NULL);
-	head = NULL;
+	if (!lst || !f || !del)
+		return (0);
+	new_list = 0;
 	while (lst)
 	{
-		new_node = ft_lstnew(f(lst->content));
-		if (!new_node)
+		tmp = f(lst -> content);
+		new = ft_lstnew(tmp);
+		if (!new)
 		{
-			ft_lstclear(&head, del);
-			lst = NULL;
-			return (NULL);
+			ft_lstclear(&new_list, del);
+			free (tmp);
+			return (0);
 		}
-		ft_lstadd_back(&head, new_node);
-		lst = lst->next;
+		ft_lstadd_back(&new_list, new);
+		lst = lst -> next;
 	}
-	return (head);
+	return (new_list);
 }
