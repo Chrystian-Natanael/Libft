bcd7489ae0e99cedbcf60014a1d7989b8ba19610
cnatanae <cnatanae@student.42sp.org.br>
2023-11-11 12:57:25 -0300

    lstnew - OK

--- source/ft_lstnew.c
+++ source/ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: cnatanae <cnatanae@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 12:56:14 by cnatanae          #+#    #+#             */
-/*   Updated: 2023/10/25 14:38:44 by cnatanae         ###   ########.fr       */
+/*   Updated: 2023/11/11 12:15:59 by cnatanae         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 t_list	*ft_lstnew(void *content)
 {
-	t_list	*n_node;
+	t_list	*new;
 
-	n_node = (t_list *)ft_calloc(1, sizeof(t_list));
-	if (!n_node)
-		return (NULL);
-	n_node->content = content;
-	n_node->next = NULL;
-	return (n_node);
+	new = (t_list *) malloc(1 * sizeof(t_list));
+	if (!new)
+		return (0);
+	new -> content = content;
+	new -> next = 0;
+	return (new);
 }
